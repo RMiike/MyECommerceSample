d1514a431048218778dbbb30e8a88946a97c4a5d
Renato Miike Alves <renato@aqua.com.br>
2021-05-03 18:52:06 -0300

    Added first mvc configuration

---
+++ src/web/MECS.WebApp.MVC/Configuration/IdentityConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MECS.WebApp.MVC.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static void AddIdentityConfig(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddCookie(opt =>
+                    {
+                        opt.LoginPath = "/signin";
+                        opt.AccessDeniedPath = "/access-denied";
+                    });
+        }
+
+        public static void UseIdentityConfig(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
