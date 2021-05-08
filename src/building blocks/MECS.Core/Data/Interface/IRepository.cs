f712127b62feac689b1fd1b008ddcee300eaef97
Renato Miike Alves <renato@aqua.com.br>
2021-05-08 12:53:22 -0300

    adicionado contexto da api de catalogo

---
+++ src/building blocks/MECS.Core/Data/Interface/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using MECS.Core.Domain.Interfaces;
+using System;
+
+namespace MECS.Core.Data.Interface
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+
+    }
+}
---
+++ src/services/MECS.Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using MECS.Core.Data.Interface;
+using MECS.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MECS.Catalog.API.Data
+{
+    public class CatalogContext : DbContext, IUnitOfWork
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> opt) : base(opt) { }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogContext).Assembly);
+        }
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
