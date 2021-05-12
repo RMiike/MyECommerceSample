e0ff67065ea1ab5eee37b8a1bdf7c814fbd9066b
Renato Miike Alves <renato@aqua.com.br>
2021-05-12 12:48:29 -0300

    added delegating handlers and refit commented implementation

--- src/web/MECS.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/MECS.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,17 +1,30 @@
 ﻿using MECS.WebApp.MVC.Extensions;
 using MECS.WebApp.MVC.Interfaces;
 using MECS.WebApp.MVC.Services;
+using MECS.WebApp.MVC.Services.Handlers;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MECS.WebApp.MVC.Configuration
 {
     public static class DependencyInjectionConfig
     {
-        public static void RegisterServices(this IServiceCollection services)
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+
             services.AddHttpClient<IAuthService, AuthService>();
-            services.AddHttpClient<ICatalogService, CatalogService>();
+
+            services.AddHttpClient<ICatalogService, CatalogService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+
+            //services.AddHttpClient("Refit",
+            //    opt => { opt.BaseAddress = new Uri(configuration.GetSection("CatalogUrl").Value); })
+            //    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+            //    .AddTypedClient(Refit.RestService.For<ICatalogServiceRefit>);
+
+
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
 
---
+++ src/web/MECS.WebApp.MVC/Interfaces/ICatalogServiceRefit.cs
@@ -0,0 +1,17 @@
+﻿using MECS.WebApp.MVC.Models;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MECS.WebApp.MVC.Interfaces
+{
+    public interface ICatalogServiceRefit
+    {
+        [Get("/catalog/products/")]
+        Task<IEnumerable<ProductViewModel>> Get();
+
+        [Get("/catalog/products/{id}")]
+        Task<ProductViewModel> Get(Guid id);
+    }
+}
