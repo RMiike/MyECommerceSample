9d2a695aafdce1b3d3c76f134ba5520db2a947a0
Renato Miike <56553271+RMiike@users.noreply.github.com>
2021-07-02 19:59:22 -0300

    adicionado voucher ao bff (#38)
    Co-authored-by: Renato Miike Alves

--- src/api gateways/MECS.BFF.Purshases/Services/OrderService.cs
+++ src/api gateways/MECS.BFF.Purshases/Services/OrderService.cs
@@ -1,13 +1,16 @@
-﻿using MECS.Core.Domain.Entities;
+﻿using MECS.BFF.Purshases.Models;
+using MECS.Core.Domain.Entities;
 using Microsoft.Extensions.Options;
 using System;
+using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace MECS.BFF.Purshases.Services
 {
     public interface IOrderService
     {
-
+        Task<VoucherDTO> GetVoucherByCode(string code);
     }
     public class OrderService : Service, IOrderService
     {
@@ -18,6 +21,19 @@ namespace MECS.BFF.Purshases.Services
         {
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.OrderUrl);
+        }
+
+        public async Task<VoucherDTO> GetVoucherByCode(string code)
+        {
+
+            var response = await _httpClient.GetAsync($"/api/voucher/{code}");
+            if (response.StatusCode == HttpStatusCode.NotFound)
+                return null;
+
+            TreateErrorsResponse(response);
+
+            return await DeserializeObjectResponse<VoucherDTO>(response);
+
         }
     }
 }
